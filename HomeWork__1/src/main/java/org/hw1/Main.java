d7ec2858dced0ab2ef0ebd82b91f25efece11d83
Viktor Velychko <cezn.vi@gmail.com>
2024-01-23 20:44:52 +0200

    task_2

--- HomeWork__1/src/main/java/org/hw1/Main.java
+++ HomeWork__1/src/main/java/org/hw1/Main.java
@@ -1,9 +1,13 @@
 package org.hw1;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        task1();
+        //task1();
+
+        task2();
     }
 
 
@@ -12,5 +16,28 @@ public class Main {
         String str = "“Your time is limited,\n\tso don’t waste it\n\t\tliving someone else’s life”\n\t\t\tSteve Jobs";
         System.out.println(str);
     }
+
+    //// --------- TASK 2 SOLUTION ---------
+    public static void task2(){
+        System.out.println("Введите два числа, \n" +
+                "где - 1е число это значение\n" +
+                "2е число это процент от первого числа\n");
+        Scanner scanner = new Scanner(System.in);
+
+        try
+        {
+            double number = scanner.nextDouble();
+            double percent = scanner.nextDouble();
+            System.out.println(number*percent/100);
+        }
+        catch (InputMismatchException ex)
+        {
+            System.out.println("Введеные данные не являются числами");
+            task2();
+        }
+
+
+    }
+
 
 }
