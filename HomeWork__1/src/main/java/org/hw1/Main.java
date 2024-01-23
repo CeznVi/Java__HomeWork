1ec2e8d58c25f17a6063d542d7d937e1dec35796
Viktor Velychko <cezn.vi@gmail.com>
2024-01-23 20:12:42 +0200

    task_1

---
+++ HomeWork__1/src/main/java/org/hw1/Main.java
@@ -0,0 +1,16 @@
+package org.hw1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        task1();
+    }
+
+
+    //// --------- TASK 1 SOLUTION ---------
+    public static void task1(){
+        String str = "“Your time is limited,\n\tso don’t waste it\n\t\tliving someone else’s life”\n\t\t\tSteve Jobs";
+        System.out.println(str);
+    }
+
+}
