7b6269d9403c448bcf5094a0ed79237c18938fa7
Viktor Velychko <cezn.vi@gmail.com>
2024-01-23 22:08:41 +0200

    task_4

--- HomeWork__1/src/main/java/org/hw1/Main.java
+++ HomeWork__1/src/main/java/org/hw1/Main.java
@@ -1,4 +1,6 @@
 package org.hw1;
+import exeptions.SixDigitNumberException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,13 +8,17 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         //task1();
 
         //task2();
 
-        task3();
+        //task3();
+
+        task4();
     }
 
 
@@ -27,8 +33,6 @@ public class Main {
         System.out.println("Введите два числа, \n" +
                 "где - 1е число это значение\n" +
                 "2е число это процент от первого числа\n");
-        Scanner scanner = new Scanner(System.in);
-
         try
         {
             double number = scanner.nextDouble();
@@ -46,8 +50,6 @@ public class Main {
 
     //// --------- TASK 3 SOLUTION ---------
     public static void task3(){
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите три числа и получите результат \n");
         try {
             int fNum = scanner.nextInt();
@@ -57,6 +59,32 @@ public class Main {
         }
         catch (InputMismatchException ex) {
             System.out.println("Введеные данные - не целое число\n");
+        }
+
+
+    }
+
+    //// --------- TASK 4 SOLUTION ---------
+    public static void task4(){
+        System.out.println("Введите шестизначное число\n");
+        try {
+            int num = scanner.nextInt();
+
+            if(num/100000 < 1 || num/100000 >10) { throw new SixDigitNumberException("Введенное число не шестизначное"); }
+
+            int firstNum = num / 100000;
+            int sixNum = num % 10;
+            int twoNum = (num / 10000) % 10;
+            int fiveNum = (num % 100) / 10;
+            int centerNum = (num % 10000) / 100;
+
+            System.out.printf("Введенное число = " + num +"\n");
+            System.out.printf("Ответ = " + sixNum + fiveNum + centerNum + twoNum + firstNum);
+
+        } catch (SixDigitNumberException sixDigitNumberException) {
+            System.out.println(sixDigitNumberException.getMessage());
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Введенное значение не число");
         }
 
 
