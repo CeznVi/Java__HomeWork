8fe320cfeed50e8ccf625062a3745eec473405d0
Viktor Velychko <cezn.vi@gmail.com>
2024-02-02 21:57:16 +0200

    task_1 finally

--- HomeWork__1/src/main/java/org/hw/Main.java
+++ HomeWork__1/src/main/java/org/hw/Main.java
@@ -2,14 +2,17 @@ package org.hw;
 
 
 import org.hw.hw1.homework1;
+import org.hw.hw2.homework2;
 
 public class Main {
 
     public static void main(String[] args) {
 
         ///HOME WORK 1 -- start
-        Runnable homeWork = new homework1();
+        //Runnable homeWork = new homework1();
 
+        ///HOME WORK 2 -- start
+        Runnable homeWork = new homework2();
 
         homeWork.run();
     }
---
+++ HomeWork__1/src/main/java/org/hw/hw2/homework2.java
@@ -0,0 +1,21 @@
+package org.hw.hw2;
+
+public class homework2 implements Runnable{
+    @Override
+    public void run() {
+
+        ////TASK 1
+        SimulatorSeaBoatStation simulationSeaBoatStation = new SimulatorSeaBoatStation();
+        simulationSeaBoatStation.runSimulation();
+
+
+
+
+    }
+
+
+
+
+
+
+}
