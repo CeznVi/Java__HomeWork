1b9954ec1990c2f5a304ffa5f407867a3359bc6e
Viktor Velychko <cezn.vi@gmail.com>
2024-02-11 20:13:27 +0200

    create project

---
+++ hw_spring_intro/src/main/java/org/example/hw_spring_intro/HwSpringIntroApplication.java
@@ -0,0 +1,13 @@
+package org.example.hw_spring_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpringIntroApplication.class, args);
+    }
+
+}
